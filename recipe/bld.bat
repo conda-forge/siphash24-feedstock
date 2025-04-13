07c9691b6baa251d034ea5da6ba3b4e18a581c1a
Daniele Nicolodi <daniele@grinta.net>
2025-04-13 15:33:55 +0200

    Build on Windows too

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+@echo on
+
+:: See https://github.com/conda-forge/numpy-feedstock/blob/main/recipe/bld.bat
+
+mkdir builddir
+
+:: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+%PYTHON% -m build -w -n -x -Cbuilddir=builddir
+if %ERRORLEVEL% neq 0 exit 1
+
+:: `pip install dist\numpy*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    pip install %%f
+    if %ERRORLEVEL% neq 0 exit 1
+)
